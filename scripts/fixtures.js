a5a679c8a79099b12869fc1c19f9550b98b0ad2f
PriyaBachoo <priyakbachoo@gmail.com>
2017-07-31 06:35:09 -0700

    Checkpoint-31-jQuery-Next/Previous-buttons:completed

---
+++ scripts/fixtures.js
@@ -0,0 +1,47 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+
+
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1903',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+
+
+  var albumAdele = {
+      title: '25',
+      artist: 'Adele',
+      label: 'XL Recordings',
+      year: '2015',
+      albumArtUrl: 'assets/images/album_covers/20.png',
+      songs: [
+          { title: 'Hello', duration: '4:55' },
+          { title: 'Send my love', duration: '3:43' },
+          { title: 'Remedy', duration: '4:05'},
+          { title: 'River Lea', duration: '3:45' },
+          { title: 'All I ask', duration: '4:32'}
+      ]
+  };
